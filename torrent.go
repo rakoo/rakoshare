21a12dcc4d9f85386733ff949fce49d432940cd9
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2014-04-13 19:58:55 +0200

    Move metadata in its own file

--- torrent.go
+++ torrent.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
-	"crypto/sha1"
 	"errors"
 	"flag"
 	"fmt"
@@ -1038,74 +1036,6 @@ type MetadataMessage struct {
 	MsgType   uint8 "msg_type"
 	Piece     uint  "piece"
 	TotalSize uint  "total_size"
-}
-
-func (t *TorrentSession) DoMetadata(msg []byte, p *peerState) {
-	// We need a buffered reader because the raw data is put directly
-	// after the bencoded data, and a simple reader will get all its bytes
-	// eaten. A buffered reader will keep a reference to where the
-	// bdecoding ended.
-	br := bufio.NewReader(bytes.NewReader(msg))
-	var message MetadataMessage
-	err := bencode.Unmarshal(br, &message)
-	if err != nil {
-		log.Println("Error when parsing metadata: ", err)
-		return
-	}
-
-	mt := message.MsgType
-	switch mt {
-	case METADATA_REQUEST:
-		//TODO: Answer to metadata request
-	case METADATA_DATA:
-
-		var piece bytes.Buffer
-		_, err := io.Copy(&piece, br)
-		if err != nil {
-			log.Println("Error when getting metadata piece: ", err)
-			return
-		}
-		t.si.ME.Pieces[message.Piece] = piece.Bytes()
-
-		finished := true
-		for idx, data := range t.si.ME.Pieces {
-			if len(data) == 0 {
-				p.sendMetadataRequest(idx)
-				finished = false
-			}
-		}
-
-		if !finished {
-			break
-		}
-
-		log.Println("Finished downloading metadata!")
-		var full bytes.Buffer
-		for _, piece := range t.si.ME.Pieces {
-			full.Write(piece)
-		}
-		b := full.Bytes()
-
-		// Verify sha
-		sha := sha1.New()
-		sha.Write(b)
-		actual := string(sha.Sum(nil))
-		if actual != t.m.InfoHash {
-			log.Println("Invalid metadata")
-			log.Printf("Expected %s, got %s\n", t.m.InfoHash, actual)
-		}
-
-		metadata := string(b)
-		err = saveMetaInfo(metadata)
-		if err != nil {
-			return
-		}
-		t.reload(metadata)
-	case METADATA_REJECT:
-		log.Printf("%d didn't want to send piece %d\n", p.address, message.Piece)
-	default:
-		log.Println("Didn't understand metadata extension type: ", mt)
-	}
 }
 
 func (t *TorrentSession) sendRequest(peer *peerState, index, begin, length uint32) (err error) {
