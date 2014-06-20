21162b2bde10967789ae46016e48bbdccd757897
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2014-06-21 00:06:00 +0200

    Fix test

--- dirwatch_test.go
+++ dirwatch_test.go
@@ -1,35 +1,70 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
-
-	"github.com/zeebo/bencode"
 )
+
+type testVector struct {
+	dir             string
+	expectedTorrent string
+}
+
+var vecs = []testVector{
+	{
+		dir:             "testData/dsl-4.4.10.iso",
+		expectedTorrent: "testData/dsl-1M.torrent",
+	},
+}
 
 func TestTorrentify(t *testing.T) {
 	if testing.Short() {
 		t.Skip("This test requires the iso")
 	}
 
-	if _, err := os.Stat("testData/dsl-4.4.10.iso"); err != nil && os.IsNotExist(err) {
-		t.Fatal("You need to download the iso relative to a.torrent to run this test")
+	for _, vec := range vecs {
+		if _, err := os.Stat("testData/dsl-4.4.10.iso"); err != nil && os.IsNotExist(err) {
+			t.Fatal("You need to download the iso relative to a.torrent to run this test")
+		}
+
+		actualMeta, err := createMeta(vec.dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		actualPieces := split(actualMeta.Info.Pieces)
+
+		expected, err := ioutil.ReadFile(vec.expectedTorrent)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expectedMeta, err := NewMetaInfoFromContent(expected)
+		if err != nil {
+			t.Fatal(err)
+		}
+		expectedPieces := split(expectedMeta.Info.Pieces)
+
+		for i, exp := range expectedPieces {
+			if actualPieces[i] != exp {
+				fmt.Printf("%x - %x\n", exp, actualPieces[i])
+			}
+		}
+
 	}
+}
 
-	meta, err := createMeta("testData")
-	if err != nil {
-		t.Fatal(err)
+func split(pieces string) []string {
+	out := make([]string, len(pieces)/sha1.Size)
+	for i := 0; i < len(out); i++ {
+		from := i * sha1.Size
+		to := (i + 1) * sha1.Size
+		if i == len(out)-1 {
+			to = len(pieces)
+		}
+		out[i] = pieces[from:to]
 	}
-
-	out, _ := os.Create("outf")
-	bencode.NewEncoder(out).Encode(meta)
-
-	expected := "f6dbbaa994438a0160d2b6f0f6129ba8a9f2d3ab"
-	if fmt.Sprintf("%x", meta.InfoHash) != expected {
-		t.Fatalf("Wrong infohash; expected %s, got %x",
-			expected,
-			meta.InfoHash)
-	}
-
+	return out
 }
